bd0521a958e50210405d5dc4622178ad8f7f02d7
Paul R Alexander <palexander@stanford.edu>
2013-02-14 12:38:13 -0800

    Optional file download, processing all versions

--- settings.sample.rb
+++ settings.sample.rb
@@ -1,12 +1,13 @@
-REST_URL          = "http://rest.bioontology.org/bioportal"
-API_KEY           = ""
-DOWNLOAD_FILES    = true
+REST_URL              = "http://rest.bioontology.org/bioportal"
+API_KEY               = ""
+DOWNLOAD_FILES        = true
+ALL_ONTOLOGY_VERSIONS = false
 # MySQL host for users.
-USERS_DB_HOST     = ""
-USERS_DB_USERNAME = ""
-USERS_DB_PASSWORD = ""
+USERS_DB_HOST         = ""
+USERS_DB_USERNAME     = ""
+USERS_DB_PASSWORD     = ""
 # MySQL host for projects and reviews.
-ROR_DB_HOST = ""
-ROR_DB_USERNAME = ""
-ROR_DB_PASSWORD = ""
+ROR_DB_HOST           = ""
+ROR_DB_USERNAME       = ""
+ROR_DB_PASSWORD       = ""
 
