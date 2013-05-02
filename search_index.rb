8b714a7b5a4f4f00a9f2a7e1b4a6cab2ca72fa6b
Michael Dorf <mdorf@stanford.edu>
2013-05-02 16:55:23 -0700

    added error handling to search indexing script

--- search_index.rb
+++ search_index.rb
@@ -5,8 +5,12 @@ require 'progressbar'
 
 submissions = LinkedData::Models::OntologySubmission.where(summaryOnly: false, submissionStatus: {code: "RDF"})
 pbar = ProgressBar.new("Indexing Ontologies for search", submissions.length)
-
+logger = Logger.new("logs/indexing.log")
 submissions.each do |s|
-  s.index Logger.new("logs/indexing.log")
+  begin
+    s.index logger
+  rescue Exception => e
+    logger.error e
+  end
   pbar.inc
 end
