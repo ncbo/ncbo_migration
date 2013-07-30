a3630a6cfcea3965ef696ef40bb18c0c0a0d265b
msalvadores <msalvadores@gmail.com>
2013-07-30 16:03:31 -0700

    script to migrate mappings from repository

---
+++ mappings_from_batch_files.rb
@@ -0,0 +1,50 @@
+puts "Loading submissions ..."
+
+BatchProcess = LinkedData::Mappings::BatchProcess
+
+attributes = LinkedData::Models::OntologySubmission.attributes + [ontology: [:acronym]]
+submissions = LinkedData::Models::OntologySubmission
+                                         .where(submissionStatus: {code: "RDF"}, 
+                                                summaryOnly: false)
+                                         .include(attributes)
+                                         .to_a
+
+
+puts "Initial submission bulk #{submissions.length}"
+
+mappings_to_process = {}
+submissions.each do |s|
+  next if !only_mappings.empty? && only_mappings.index(s.ontology.acronym) == nil
+  if mappings_to_process[s.ontology.acronym]
+    if mappings_to_process[s.ontology.acronym].submissionId < s.submissionId
+     mappings_to_process[s.ontology.acronym] = s
+    end
+  else
+    mappings_to_process[s.ontology.acronym] = s
+  end
+end
+
+puts "I am going to flush the mapping graphs"
+puts "sure ? (type !!! to exit ctrl-D to continue)"
+binding.pry
+
+Goo.sparql_data_client.delete_graph(LinkedData::Models::Mapping.type_uri)
+Goo.sparql_data_client.delete_graph(LinkedData::Models::TermMapping.type_uri)
+Goo.sparql_data_client.delete_graph(LinkedData::Models::MappingProcess.type_uri)
+
+acronyms_sorted = mappings_to_process.keys.sort
+subp = ProgressBar.new("Processing",mappings_to_process.length)
+acronyms_sorted.each do |acr|
+    submission = mappings_to_process[acr]
+    batch_triples = File.join([BatchProcess.mappings_ontology_folder(submission.ont),
+                                "batch_triples.nq"])
+    if File.exist?(batch_triples)
+      begin
+        Goo.sparql_data_client.append_triples_from_file(
+                      RDF::URI.new("http://bogus"), batch_triples, "text/x-nquads")
+      rescue => e
+        puts "Error uploading #{batch_triples}: #{e}"
+      end
+    end
+    subp.inc
+end
