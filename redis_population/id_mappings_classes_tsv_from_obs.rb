284d64a089585a80072eb1a4584629d4e18b3600
Paul R Alexander <palexander@stanford.edu>
2013-09-07 11:06:33 -0700

    Set tsv file path

--- redis_population/id_mappings_classes_tsv_from_obs.rb
+++ redis_population/id_mappings_classes_tsv_from_obs.rb
@@ -3,6 +3,7 @@ require 'ncbo_resolver'
 require_relative '../settings'
 
 options = {
+  tsv_path: REPOSITORY_FOLDER + "/obs_classes.tsv",
   obs_host: OBS_DB_HOST,
   obs_username: OBS_DB_USERNAME,
   obs_password: OBS_DB_PASSWORD,
