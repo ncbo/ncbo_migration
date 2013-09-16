e96b479237bebdfe918bda42c03bf11db05e5a6b
Paul R Alexander <palexander@stanford.edu>
2013-09-16 12:09:18 -0700

    Improvements to root checking

--- quality_checks/roots.rb
+++ quality_checks/roots.rb
@@ -1,10 +1,10 @@
 require_relative '../settings.rb'
 require_relative '../helpers/rest_helper'
 
-bad_roots = [ ["ontology", "production", "new api"] ]
 puts ["ontology".ljust(15), "production".rjust(10), "new api".rjust(10)].join("\t\t")
 
 rest_ontologies = RestHelper.ontologies
+rest_ontologies.sort! {|a,b| a.abbreviation.downcase <=> b.abbreviation.downcase}
 rest_ontologies.each do |rest_ont|
   begin
     rest_roots = RestHelper.roots(rest_ont.id).length
@@ -16,15 +16,14 @@ rest_ontologies.each do |rest_ont|
     next
   end
   
-  sub = LinkedData::Models::Ontology.find(rest_ont.abbreviation).first.latest_submission rescue next
+  ont = LinkedData::Models::Ontology.find(rest_ont.abbreviation).first
+  next unless ont
+  sub = ont.latest_submission rescue next
   next unless sub
-  roots = sub.roots(nil,false,true).length
+  roots = sub.roots.length
   
-  if rest_roots.to_f / roots.to_f < 0.9 || rest_roots.to_f / roots.to_f > 1.1
-    bad_roots << [rest_ont.abbreviation, rest_roots, roots]
-    bad = "***"
-  end
+  difference = rest_roots.to_f / roots.to_f
+  bad = "*" if difference < 0.9 || difference > 1.1
   puts ["#{rest_ont.abbreviation} #{bad}".ljust(15), rest_roots.to_s.rjust(10), roots.to_s.rjust(10)].join("\t\t")
 end
 
-puts bad_roots.each {|row| "#{row.join("\t\t")}\n"}
