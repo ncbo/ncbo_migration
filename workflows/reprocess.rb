4e6098a7e6edc88f40ff4ff83210ae05f52de8f9
msalvadores <msalvadores@gmail.com>
2013-09-26 17:19:12 -0700

    script reprocesses tested

--- workflows/reprocess.rb
+++ workflows/reprocess.rb
@@ -7,39 +7,24 @@ require 'progressbar'
 acronyms = ["CCO"]
 
 submissions = []
-acronym.each |acr| do 
-  submissions << LinkedData::Models::Ontology.find(acr).latest_submission(status: :any)
+acronyms.each do |acr|
+  submissions << LinkedData::Models::Ontology.find(acr).first.latest_submission(status: :any)
 end
-binding.pry
 
 puts "", "Parsing #{submissions.length} submissions..."
 pbar = ProgressBar.new("Parsing", submissions.length)
 FileUtils.mkdir_p("./parsing")
 submissions.each do |s|
-  submissions.bring_remaining
-  log_file = File.open("./parsing/parsing_#{os.ontology.acronym}.log", "w")
+  s.bring_remaining
+  s.bring(ontology: [:acronym])
+  log_file = File.open("./parsing/parsing_#{s.ontology.acronym}.log", "w")
   logger = Logger.new(log_file)
   logger.level = Logger::DEBUG
   begin
-    reasoning = !disable_reasoning_for.include?(os.ontology.acronym)
-    os.process_submission(logger,
+    binding.pry
+    s.process_submission(logger,
                           process_rdf: true, index_search: false,
                           run_metrics: false, reasoning: true)
-end
-
-
-ontologies_to_parse.each do |os|
-  next if os.ontology.summaryOnly
-
-  log_file = File.open("./parsing/parsing_#{os.ontology.acronym}.log", "w")
-  logger = Logger.new(log_file)
-  logger.level = Logger::DEBUG
-
-  begin
-    os.process_submission(logger,
-                          process_rdf: true, index_search: false,
-                          run_metrics: false, reasoning: reasoning)
-
   rescue Exception => e
     binding.pry
   end
