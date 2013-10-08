9e3966eb746dbe522592e12e073dba80c19e29c8
msalvadores <msalvadores@gmail.com>
2013-10-08 12:51:33 -0700

    get all obo submissions (latest)

--- workflows/reprocess.rb
+++ workflows/reprocess.rb
@@ -4,12 +4,31 @@ require 'logger'
 require 'progressbar'
 
 # An array of acronyms to restrict parsing to these particular ontologies
-acronyms = ["CCO"]
+
+def get_obo_submissions
+  subs = []
+  LinkedData::Models::Ontology.where.include(:acronym, :summaryOnly).all.each do |ont|
+    if !ont.summaryOnly
+      sub = ont.latest_submission(status: :any)
+      if sub
+        sub.bring(:hasOntologyLanguage)
+        if sub.hasOntologyLanguage.obo?
+          subs << sub
+        end
+      else
+        puts "OBO ontology with no submissions #{ont.id.to_s}"
+      end
+    end
+  end
+  return subs
+end
 
 submissions = []
+acronyms = ["CCO"]
 acronyms.each do |acr|
   submissions << LinkedData::Models::Ontology.find(acr).first.latest_submission(status: :any)
 end
+#submissions = get_obo_submissions
 
 puts "", "Parsing #{submissions.length} submissions..."
 pbar = ProgressBar.new("Parsing", submissions.length)
@@ -21,7 +40,6 @@ submissions.each do |s|
   logger = Logger.new(log_file)
   logger.level = Logger::DEBUG
   begin
-    binding.pry
     s.process_submission(logger,
                           process_rdf: true, index_search: false,
                           run_metrics: false, reasoning: true)
