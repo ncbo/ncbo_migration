da4227a45a9c91d4889b1a5cdb9bb68c447549ed
msalvadores <msalvadores@gmail.com>
2014-08-20 18:14:42 -0700

    script to migrate explicit rest mappings to implicit

---
+++ workflows/dump_explicit_rest_mappings.rb
@@ -0,0 +1,94 @@
+require_relative '../settings'
+
+def dump_all_rest_mappings() 
+  rest_predicate = LinkedData::Mappings.mapping_predicates["REST"][0]
+  procs = LinkedData::Models::MappingProcess.where
+      .include(LinkedData::Models::MappingProcess.attributes)
+      .all.select { |x| !x.date.nil? }
+  
+  procs.each do |p|
+    pgraph  = Goo::SPARQL::Triples.model_delete_triples(p)
+    pgraph[0].each do |triple|
+        dump_triple(triple,LinkedData::Models::MappingProcess.type_uri.to_s)
+    end
+    mapping_ids = []
+    qmappings = <<-eos
+  SELECT ?s
+  WHERE { ?s <http://data.bioontology.org/metadata/process> <#{p.id.to_s}> .
+  }
+  eos
+    epr = Goo.sparql_query_client(:main)
+    mapping_ids = []
+    epr.query(qmappings).each do |sol|
+        mapping_ids << sol[:s]
+    end
+    mapping_ids.each do |mapping_id|
+    qterms = <<-eos
+  SELECT ?termId ?ontId ?classId
+  WHERE { <#{mapping_id}> 
+          <http://data.bioontology.org/metadata/terms> ?termId .
+      ?termId <http://data.bioontology.org/metadata/ontology> ?ontId .
+      ?termId <http://data.bioontology.org/metadata/term> ?classId .
+  }
+  eos
+      urn_classes = []
+      classes = []
+      latest = nil
+      epr.query(qterms).each do |sol_term|
+          ontId, classId = sol_term[:ontId], sol_term[:classId]
+          ont = LinkedData::Models::Ontology
+                  .find(ontId).include(:acronym).first
+          if ont.nil?
+            break
+          end
+          acronym = ont.acronym
+          latest = ont.latest_submission
+          if latest.nil?
+            break
+          end
+          cls = LinkedData::Models::Class.find(classId).in(latest).first
+          if cls.nil?
+            next
+          end
+          classes << cls
+          urn_classes << RDF::URI.new(
+                    LinkedData::Models::Class.urn_id(acronym,classId.to_s))
+
+      end
+      if urn_classes.length == 2
+        backup_mapping = LinkedData::Models::RestBackupMapping.new
+        backup_mapping.uuid = UUID.new.generate
+        backup_mapping.process = p
+        backup_mapping.class_urns = urn_classes
+        mgraph = Goo::SPARQL::Triples.model_update_triples(backup_mapping)
+        mgraph[0].each do |triple|
+          dump_triple(triple,LinkedData::Models::RestBackupMapping.type_uri.to_s)
+        end
+        classes.each do |cls| 
+          triple =  "<#{latest.id.to_s}>" +
+                    " <#{cls.id.to_s}>" + 
+                    " <#{rest_predicate}>" +
+                    " <#{backup_mapping.id.to_s}> ."
+          puts triple
+          dump_triple([cls.id.to_s,rest_predicate,backup_mapping.id.to_s], latest.id.to_s)
+        end
+      end
+    end
+
+  end
+end
+
+def dump_triple(triple,graph)
+  quad = []
+  3.times do |i|
+    if triple[i].respond_to?(:to_ntriples)
+      quad << triple[i].to_ntriples
+    else
+      quad << "<#{triple[i].to_s}>"
+    end
+  end
+  quad << "<#{graph.to_s}>"
+  quad << "."
+  puts quad.join(" ")
+end
+dump_all_rest_mappings
